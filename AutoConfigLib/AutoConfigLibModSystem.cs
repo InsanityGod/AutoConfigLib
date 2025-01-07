1f36bb73323fb539f20d1c5b5f95b0a4ea34aebb
_Kristof_ <krizziewissie@gmail.com>
2025-01-07 14:30:18 +0100

    update modInfo and TODO

--- AutoConfigLib/AutoConfigLibModSystem.cs
+++ AutoConfigLib/AutoConfigLibModSystem.cs
@@ -19,6 +19,8 @@ namespace AutoConfigLib
                 harmony.PatchAllUncategorized();
                 PatchConfigLoadingCode.PatchConfigStuff(api, harmony);
             }
+
+            //TODO world config
             base.StartPre(api);
         }
 
