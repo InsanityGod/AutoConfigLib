87c537d783eb7fa8b7c99de478563f1dec4c12e0
_Kristof_ <krizziewissie@gmail.com>
2025-02-12 14:07:36 +0100

    interface start

---
+++ AutoConfigLib/Custom/ICustomEditor.cs
@@ -0,0 +1,34 @@
+﻿using ConfigLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoConfigLib.Custom
+{
+    public interface ICustomEditor
+    {
+        public ControlButtons SupportedButtons { get; }
+
+        public void Save()
+        {
+            //Empty stub
+        }
+
+        public void Restore()
+        {
+            //Empty stub
+        }
+
+        public void Reload()
+        {
+            //Empty stub
+        }
+
+        public void Defaults()
+        {
+            //Empty stub
+        }
+    }
+}
